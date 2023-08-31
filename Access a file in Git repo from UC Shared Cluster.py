52f84c291a367f75a6c63a65de813d943e6fdad6
PawaritL <PawaritL>
2023-08-31 06:35:59 +0000

    local filesystem API

--- Access a file in Git repo from UC Shared Cluster.py
+++ Access a file in Git repo from UC Shared Cluster.py
@@ -6,6 +6,18 @@ current_working_dir
 
 # COMMAND ----------
 
+df = spark.read.format("csv").option("header", "true").option("delimiter", ";").load(f"file://{current_working_dir}/sample.csv")
+display(df)
+
+# COMMAND ----------
+
+import pandas as pd
+
+# example
+pd.read_csv("/Workspace/Repos/pawarit.laosunthara@databricks.com/files_in_repos/sample.csv", sep=";")
+
+# COMMAND ----------
+
 import pandas as pd
 
 # you should be able to read arbitrary files using any functions/libraries in the Python ecosystem
